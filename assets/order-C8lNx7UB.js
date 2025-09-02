38c6af140bbc6ffca5c1d8510f0436b0022b0ba9
Ho-67 <ho67cnl@gmail.com>
2025-09-02 06:36:37 +0000

    Deploying to gh-pages from @ Ho-67/SANVIA_FRONT@e3d04ea03432a3724f880836fc875de29a12d32c 🚀

--- assets/order-C8lNx7UB.js
+++
@@ -1,1 +0,0 @@
-import{aj as r}from"./index-CY7g7pVQ.js";const o={create(e){return r.apiAuth.post("/order",e)},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{o};
